de13b3782e93ccd33138f31d33adeaac2dcd8c34
Richard Hull <rm_hull@yahoo.co.uk>
2016-10-08 14:48:45 +0100

    Add cljfmt check in travis config

--- project.clj
+++ project.clj
@@ -20,6 +20,7 @@
       :global-vars {*warn-on-reflection* true}
       :plugins [
         [lein-codox "0.10.0"]
+        [lein-cljfmt "0.5.6"]
         [lein-cloverage "1.0.7"]]
       :dependencies [
         [org.clojure/clojure "1.8.0"]]}})
