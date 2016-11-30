604c16d57908fbb78e2be27794d25100b60f036a
Richard Hull <rm_hull@yahoo.co.uk>
2016-11-30 23:30:33 +0000

    Add analytics to generated API docs

--- project.clj
+++ project.clj
@@ -13,15 +13,17 @@
   :codox {
     :source-paths ["src"]
     :output-path "doc/api"
-    :source-uri "http://github.com/rm-hull/infix/blob/master/{filepath}#L{line}"  }
+    :source-uri "http://github.com/rm-hull/infix/blob/master/{filepath}#L{line}"
+    :themes [:default [:google-analytics {:tracking-code "UA-39680853-6" }]]}
   :min-lein-version "2.6.1"
   :profiles {
     :dev {
       :global-vars {*warn-on-reflection* true}
       :plugins [
-        [lein-codox "0.10.1"]
+        [lein-codox "0.10.2"]
         [lein-cljfmt "0.5.6"]
         [lein-cloverage "1.0.9"]]
       :dependencies [
-        [org.clojure/clojure "1.8.0"]]}})
+        [org.clojure/clojure "1.8.0"]
+        [google-analytics-codox-theme "0.1.0"]]}})
 
