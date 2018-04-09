aea4c4a2e7359618aaa640fd424426aa3c78868a
Richard Hull <rm_hull@yahoo.co.uk>
2018-04-09 22:30:41 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject rm-hull/infix "0.3.1"
+(defproject rm-hull/infix "0.3.2"
   :description "A small Clojure library for expressing LISP expressions as infix rather than prefix notation"
   :url "https://github.com/rm-hull/infix"
   :license {
     :name "The MIT License (MIT)"
     :url "http://opensource.org/licenses/MIT"}
   :dependencies [
-    [rm-hull/jasentaa "0.2.4"]]
+    [rm-hull/jasentaa "0.2.5"]]
   :scm {:url "git@github.com:rm-hull/infix.git"}
   :vcs :git
   :source-paths ["src"]
@@ -15,7 +15,7 @@
     :output-path "doc/api"
     :source-uri "http://github.com/rm-hull/infix/blob/master/{filepath}#L{line}"
     :themes [:default [:google-analytics {:tracking-code "UA-39680853-6" }]]}
-  :min-lein-version "2.6.1"
+  :min-lein-version "2.8.1"
   :profiles {
     :dev {
       :global-vars {*warn-on-reflection* true}
